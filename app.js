4e9358500372a268d5ce2962ba33aa7834581fa9
AlayaZarya <zhalilova4872@gmail.com>
2018-11-15 02:30:24 +0300

    commit 5

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const router 	   = express.Router();
 const port 	   = process.env.PORT || 8080;
 app.use(bodyParser.json());
 //app.use(logger('dev'));
-var AuthController = require('./auth/authController');
-var MeetController = require('./meets/meetController');
+var AuthController = require('./auth/AuthController');
+var MeetController = require('./meets/MeetController');
 
 app.use('/api/auth', AuthController);
 app.use('/api/meets', MeetController);
