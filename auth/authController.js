7134b181b147a74e29763e145516dd753780d0ec
AlayaZarya <zhalilova4872@gmail.com>
2018-11-15 23:13:00 +0300

    commit

--- auth/authController.js
+++ auth/authController.js
@@ -10,7 +10,7 @@ var VerifyToken = require('./verifyToken');
 
 var code;
 
-router.post('/register', function(req, res) {
+router.post('/authenticate', function(req, res) {
   let telephone = req.body.telephone;
   if(telephone) {
     let sql = "SELECT * FROM users WHERE telephone = '"+telephone+"' ";
@@ -20,10 +20,10 @@ router.post('/register', function(req, res) {
           res.status(500).json({message: "Invalid Request"});
         }
         else if(result.length > 0) {
-          res.status(409).json ({message: "User Already Registered! "});
+          res.status(409).json ({auth: true, message: "343631"});
         }
         else {
-          res.json ({message: "343631"});
+          res.json ({auth: false, message: "343631"});
         }
     });
   } else {
@@ -31,7 +31,7 @@ router.post('/register', function(req, res) {
   }
 });
 
-router.post('/confirmSms', function(req, res) {
+router.post('/registerConfirmSms', function(req, res) {
   let code = req.body.code;
   let telephone = req.body.telephone;
   if(code == "343631") {
@@ -47,7 +47,7 @@ router.post('/confirmSms', function(req, res) {
               var token = jwt.sign({ id: result.insertId }, config.secret, {
                 expiresIn: 86400 // expires in 24 hours
               });
-              res.status(200).json({ auth: true, token: token, id: result.insertId });
+              res.status(200).json({ auth: true, token: token, id: result.insertId, message: "User successfully registered. " });
             }
         });
 
@@ -72,27 +72,30 @@ router.post('/add_register', function(req, res) {
   });
 });
 
-router.post('/login', function(req, res) {
-  let telephone = req.body.telephone;
-  let sql = "SELECT  * FROM users WHERE telephone = '"+telephone+"'";
-  connection.query(sql, (err, result) => {
-    if (err) return res.status(500).json({message: "Error on the server."});
-    else if (result.length > 0) return res.json ({message: "343631"});
-    else return res.status(404).json({message: "No user found."});
-  });
-});
+// router.post('/login', function(req, res) {
+//   let telephone = req.body.telephone;
+//   let sql = "SELECT  * FROM users WHERE telephone = '"+telephone+"'";
+//   connection.query(sql, (err, result) => {
+//     if (err) return res.status(500).json({message: "Error on the server."});
+//     else if (result.length > 0) return res.json ({message: "343631"});
+//     else return res.status(404).json({message: "No user found."});
+//   });
+// });
 
 router.post('/loginConfirmSms', function(req, res) {
   let code = req.body.code;
   let telephone = req.body.telephone;
+  var userId;
   if(code != "343631") return res.status(401).send({ auth: false, token: null, message: "Wrong code" });
-  connection.query ("SELECT * from users WHERE telephone = '" +telephone+ "'", (err, user) => {
+  connection.query ("SELECT user_id from users WHERE telephone = '" +telephone+ "'", (err, user) => {
       if (err) return res.status(500).json({message: "Error on the server."});
       else {
+        userId = user[0].user_id;
+        console.log(userId);
   var token = jwt.sign({ id: user.user_id }, config.secret, {
     expiresIn: 86400 // expires in 24 hours
   });
-  res.status(200).send({ auth: true, token: token, id: user.user_id });
+  res.status(200).send({ auth: true, token: token, id: userId, message: "Login successfully. " });
 }
 
 
@@ -115,7 +118,7 @@ router.get('/logout', function(req, res) {
 
 router.get('/getProfile', VerifyToken, function(req, res) {
   let id = req.params.id;
-  let sql = "SELECT user_id, name, surname, telephone from users WHERE user_id = '"+21+"' ";
+  let sql = "SELECT user_id, name, surname, telephone from users WHERE user_id = '"+req.userId+"' ";
   connection.query(sql, (error, result) => {
     if (error) return res.status(500).json({message: "There was a problem finding the user."});
     if (!result) return res.status(404).json({message: "No user found."});
