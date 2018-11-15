1466a696dc6d27579bffaca02a06495ad61b1ac1
AlayaZarya <zhalilova4872@gmail.com>
2018-11-15 16:16:13 +0300

    rest api

--- auth/authController.js
+++ auth/authController.js
@@ -47,7 +47,7 @@ router.post('/confirmSms', function(req, res) {
               var token = jwt.sign({ id: result.insertId }, config.secret, {
                 expiresIn: 86400 // expires in 24 hours
               });
-              res.status(200).json({ auth: true, token: token });
+              res.status(200).json({ auth: true, token: token, id: result.insertId });
             }
         });
 
@@ -92,22 +92,22 @@ router.post('/loginConfirmSms', function(req, res) {
   var token = jwt.sign({ id: user.user_id }, config.secret, {
     expiresIn: 86400 // expires in 24 hours
   });
-  res.status(200).send({ auth: true, token: token });
+  res.status(200).send({ auth: true, token: token, id: user.user_id });
 }
 
 
 });
 });
 
-router.put('/updateProfile', (req, res) => {
-    let name = req.body.name;
-    let surname = req.body.surname;
-    let telephone = req.body.telephone;
-    let photo = req.body.photo;
-    let id = req.body.user_id;
-
-    let sql = ""
-});
+// router.put('/updateProfile', (req, res) => {
+//     let name = req.body.name;
+//     let surname = req.body.surname;
+//     let telephone = req.body.telephone;
+//     let photo = req.body.photo;
+//     let id = req.body.user_id;
+//
+//     let sql = ""
+// });
 
 router.get('/logout', function(req, res) {
   res.status(200).json({ auth: false, token: null });
@@ -115,7 +115,7 @@ router.get('/logout', function(req, res) {
 
 router.get('/getProfile', VerifyToken, function(req, res) {
   let id = req.params.id;
-  let sql = "SELECT user_id, name, surname, telephone from users WHERE user_id = '"+req.userId+"' ";
+  let sql = "SELECT user_id, name, surname, telephone from users WHERE user_id = '"+21+"' ";
   connection.query(sql, (error, result) => {
     if (error) return res.status(500).json({message: "There was a problem finding the user."});
     if (!result) return res.status(404).json({message: "No user found."});
