9c1fb75ec8a890270bdd2c19efd32f6b330ae3dc
AlayaZarya <zhalilova4872@gmail.com>
2018-11-17 02:49:14 +0300

    mysql to another hosting

--- app.js
+++ app.js
@@ -4,23 +4,23 @@ const bodyParser = require('body-parser');
 const router = express.Router();
 const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
-const connection = require('./db/db_config');
-var databaseOptions = require('./db/db_config');
-function handleDisconnect(conn) {
-  conn.on('error', function(err) {
-    if (!err.fatal) {
-      return;
-    }
-    if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
-      throw err;
-    }
-    console.log('Re-connecting lost connection: ' + err.stack);
-    connection2 = mysql.createConnection(databaseOptions);
-    handleDisconnect(connection2);
-    connection2.connect();
-  });
-}
-setInterval(function() {handleDisconnect(connection);}, 60000);
+// const connection = require('./db/db_config');
+// var databaseOptions = require('./db/db_config');
+// function handleDisconnect(conn) {
+//   conn.on('error', function(err) {
+//     if (!err.fatal) {
+//       return;
+//     }
+//     if (err.code !== 'PROTOCOL_CONNECTION_LOST') {
+//       throw err;
+//     }
+//     console.log('Re-connecting lost connection: ' + err.stack);
+//     connection2 = mysql.createConnection(databaseOptions);
+//     handleDisconnect(connection2);
+//     connection2.connect();
+//   });
+// }
+// setInterval(function() {handleDisconnect(connection);}, 60000);
 
 app.get('/', function (req, res) {
   res.status(200).send('API works.');
--- db/db_config.js
+++ db/db_config.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 const connection = mysql.createPool({
     connectionLimit: 10,
-    host: 'us-cdbr-iron-east-01.cleardb.net',
-    user: 'b8aba776cda548',
-    password: '13e72bdc',
-    database: 'heroku_5bcdde692c5189d',
+    host: 'sql12.freemysqlhosting.net',
+    user: 'sql12265805',
+    password: 'WWbnSjuJsk',
+    database: 'sql12265805',
     port: '3306',
 });
 connection.getConnection( (err, tempCon) => {
@@ -18,14 +18,21 @@ connection.getConnection( (err, tempCon) => {
   return;
 });
 
-var databaseOptions = {
-    host     : 'us-cdbr-iron-east-01.cleardb.net',
-    database : 'heroku_5bcdde692c5189d',
-    user     : 'b8aba776cda548',
-    password : '13e72bdc',
-    port     : '3306'
-};
-module.exports = {databaseOptions: databaseOptions} ;
+// connectionLimit: 10,
+// host: 'us-cdbr-iron-east-01.cleardb.net',
+// user: 'b8aba776cda548',
+// password: '13e72bdc',
+// database: 'heroku_5bcdde692c5189d',
+// port: '3306',
+//
+// var databaseOptions = {
+//     host     : 'us-cdbr-iron-east-01.cleardb.net',
+//     database : 'heroku_5bcdde692c5189d',
+//     user     : 'b8aba776cda548',
+//     password : '13e72bdc',
+//     port     : '3306'
+// };
+// module.exports = {databaseOptions: databaseOptions} ;
 
 // function handleDisconnect(conn) {
 //   conn.on('error', function(err) {
